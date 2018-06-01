41ac74fdf71d1cd61f4b3eae7bbf4592b94d00e1
Dennis S. Durano <dennisdurano@gmail.com>
2018-06-01 18:19:21 +0800

    Update ScrumTeamApplication.java

--- src/main/java/com/example/demo/ScrumTeamApplication.java
+++ src/main/java/com/example/demo/ScrumTeamApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+*      Entry Point Class for Scrum Team Application
+*
+*/
 public class ScrumTeamApplication {
-
+/**
+*       Entry Point method for Scrum Team Application
+*/
 	public static void main(String[] args) {
 		SpringApplication.run(ScrumTeamApplication.class, args);
 	}
